b68e144f4c62c05ef8e1deff74f0d7a9b7bab7db
Glitch (balzua-game-deck-server) <none>
2019-03-15 00:40:11 +0000

    update for fetching stats or games separately

--- library/models.js
+++ library/models.js
@@ -16,7 +16,7 @@ const LibrarySchema = mongoose.Schema({
         default: true
     },
     chartScores: {
-        action: {
+        actionScore: {
             type: Number,
             required: true,
             default: 0
@@ -40,6 +40,11 @@ const LibrarySchema = mongoose.Schema({
             type: Number,
             required: true,
             default: 0
+        },
+        simScore: {
+          type: Number,
+          required: true,
+          default: 0
         }
     },
     libraryStats: {
@@ -67,10 +72,55 @@ LibrarySchema.virtual('totalGames').get(function () {
 });
 
 LibrarySchema.methods.serialize = function() {
+  
+  let chartScores = {
+    actionScore: 0,
+    puzzleScore: 0,
+    rpgScore: 0,
+    fightingScore: 0, 
+    shooterScore: 0,
+    simScore: 0
+  }
+  const genres = {
+    action: ['Action', 'Brawler', 'Vehiclular Combat', 'Platformer', 'Action-Adventure'],
+    puzzle: ['Strategy', 'Adventure', 'Real-Time Strategy', 'Card Game', 'Trivia/Board Game', 'Puzzle'],
+    rpg: ['Role-Playing', 'MMORPG'],
+    fighting: ['Fighting', 'Wrestling', 'Brawler'],
+    shooter: ['Shooter', 'Dual-Joystick Shooter', 'First-Person Shooter', 'Light-Gun Shooter', 'Shoot \'Em Up'],
+    sim: ['Sports', 'Driving/Racing', 'Simulation']
+  }
+  this.games.forEach(game => {
+    const score = game.userRating * 10    
+    game.genres.forEach(genre => {
+      if (genres.action.includes(genre)) {
+        chartScores.actionScore += score;
+      } else if (genres.puzzle.includes(genre)) {
+        chartScores.puzzleScore += score;
+      } else if (genres.rpg.includes(genre)) {
+        chartScores.rpgScore += score;
+      } else if (genres.fighting.includes(genre)) {
+        chartScores.fightingScore += score;
+      } else if (genres.shooter.includes(genre)) {
+        chartScores.shooterScore += score;
+      } else if (genres.sim.includes(genre)) {
+        chartScores.simScore += score;
+      }
+    });
+  });
+  let totalScore = Object.values(chartScores).reduce((total, num) => total + num, 0);
+  if (totalScore === 0) {
+    totalScore = 1;
+  }
   return {
     "games": this.games,
-    "chartScores": this.chartScores,
-    "platforms": this.platforms
+    "chartScores": {
+      actionScore: Math.round(chartScores.actionScore / totalScore * 100),
+      puzzleScore: Math.round(chartScores.puzzleScore / totalScore * 100),
+      rpgScore: Math.round(chartScores.rpgScore / totalScore * 100),
+      fightingScore: Math.round(chartScores.fightingScore / totalScore * 100),
+      shooterScore: Math.round(chartScores.shooterScore / totalScore * 100),
+      simScore: Math.round(chartScores.simScore / totalScore * 100)
+    }
   }
 }
 
